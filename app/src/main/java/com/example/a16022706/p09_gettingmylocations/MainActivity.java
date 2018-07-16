5c79431ab2397518e592a4ea1f09311327321029
16022706 <16022706@myrp.edu.sg>
2018-07-16 19:54:00 +0800

    Prob Statement

--- app/src/main/java/com/example/a16022706/p09_gettingmylocations/MainActivity.java
+++ app/src/main/java/com/example/a16022706/p09_gettingmylocations/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.a16022706.p09_gettingmylocations;
 
 import android.Manifest;
+import android.content.Intent;
 import android.location.Location;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,10 +17,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     Button stopBtn;
     Button checkBtn;
     TextView tvDisplay;
+
+    String folderLocation;
+
 
 
     @Override
@@ -41,6 +53,21 @@ public class MainActivity extends AppCompatActivity {
 
         checkBtn = (Button) findViewById(R.id.checkBtn);
         tvDisplay = (TextView) findViewById(R.id.tvDisplay);
+
+        createFolder();
+
+        folderLocation =
+                Environment.getExternalStorageDirectory()
+                        .getAbsolutePath() + "/Folder";
+
+        File folder = new File(folderLocation);
+        if (folder.exists() == false) {
+            boolean result = folder.mkdir();
+            if (result == true) {
+                Log.d("File Read/Write", "Folder created");
+
+            }
+        }
 
         client = LocationServices.getFusedLocationProviderClient(this);
 
@@ -88,6 +115,76 @@ public class MainActivity extends AppCompatActivity {
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                startService(i);
+
+                LocationRequest mLocationRequest = new LocationRequest();
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                mLocationRequest.setInterval(10000);
+                mLocationRequest.setFastestInterval(5000);
+                mLocationRequest.setSmallestDisplacement(100);
+                if (checkPermission() == true) {
+                    client.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+                } else {
+                    String msg = "Permission not granted to retrieve location info";
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                }
+
+            }
+        });
+
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                stopService(i);
+
+                try {
+                    String folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Folder";
+                    File targetFile = new File(folderLocation, "data.txt");
+                    FileWriter writer = new FileWriter(targetFile, true);
+                    writer.write("Hello world" + "\n");
+                    writer.flush();
+                    writer.close();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Failed to write!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Folder";
+                File targetFile = new File(folderLocation, "data.txt");
+                if (targetFile.exists() == true) {
+                    String data = "";
+                    try {
+                        FileReader reader = new FileReader(targetFile);
+                        BufferedReader br = new BufferedReader(reader);
+
+                        String line = br.readLine();
+                        while (line != null) {
+                            data += line + "\n";
+                            line = br.readLine();
+
+                        }
+                        Toast.makeText(MainActivity.this, "Done reading!", Toast.LENGTH_LONG).show();
+                        br.close();
+                        reader.close();
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "Failed to read!", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                    Log.d("Content", data);
+                }
 
             }
         });
@@ -108,6 +205,34 @@ public class MainActivity extends AppCompatActivity {
             return true;
         } else {
             return false;
+        }
+    }
+
+
+    private void createFolder(){
+        int permissionCheck_Storage = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck_Storage != PermissionChecker.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+            finish();
+        }
+
+        folderLocation = Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + "/MyFolder";
+
+        File folder = new File(folderLocation);
+        if (folder.exists() == false) {
+            boolean result = folder.mkdir();
+            if (result == true) {
+                Log.d("File Read/Write", "Folder created");
+            } else {
+                Log.e("File Read/Write", "Folder creation failed");
+
+
+            }
         }
     }
 }
